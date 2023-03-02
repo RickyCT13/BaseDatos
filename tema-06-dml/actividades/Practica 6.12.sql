f22fde2f4f3b63152b602c85c059b7ce71aed4dc
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-03-02 19:17:52 +0100

    Signed-off-by: RickyCT13 <ricardomorenocantea13@gmail.com>

--- tema-06-dml/actividades/Practica 6.12.sql
+++ tema-06-dml/actividades/Practica 6.12.sql
@@ -30,3 +30,115 @@ FROM
 HAVING YEAR(v.fecha) = 2014
 ORDER BY v.importe_total DESC
 LIMIT 3;
+
+# 3. Mostrar detalles de todos los libros editados en 2011 cuyo stock esté entre 10 y 20 unidades.
+
+SELECT 
+    l.id,
+    l.titulo,
+    a.nombre,
+    e.nombre,
+    l.stock,
+    l.precio_coste,
+    l.precio_venta
+FROM
+    libros l
+        INNER JOIN
+    autores a ON l.autor_id = a.id
+        INNER JOIN
+    editoriales e ON l.editorial_id = e.id
+WHERE
+    YEAR(l.fecha_edicion) = 2011
+HAVING l.stock BETWEEN 10 AND 20
+ORDER BY l.id ASC;
+
+# 4. Mostrar detalles de los libros relacionadas con la temática PHP y Bases de Datos, además el precio de venta sea inferior a 30 €
+
+SELECT 
+    l.id,
+    l.titulo,
+    a.nombre,
+    e.nombre,
+    l.stock,
+    l.precio_coste,
+    l.precio_venta
+FROM
+    libros l
+        INNER JOIN
+    autores a ON l.autor_id = a.id
+        INNER JOIN
+    editoriales e ON l.editorial_id = e.id
+WHERE
+    l.titulo LIKE '%PHP%'
+HAVING l.precio_venta < 30
+ORDER BY l.id ASC;
+
+# 5. Mostrar detalles de los libros junto con el margen de beneficios, es decir, la diferencia entre el precio_venta y el precio_coste.
+
+SELECT 
+    l.id,
+    l.titulo,
+    a.nombre,
+    e.nombre,
+    l.stock,
+    l.precio_coste,
+    l.precio_venta,
+    (l.precio_venta - l.precio_coste) margen_beneficios
+FROM
+    libros l
+        INNER JOIN
+    autores a ON l.autor_id = a.id
+        INNER JOIN
+    editoriales e ON l.editorial_id = e.id
+WHERE
+    e.id BETWEEN 5 AND 6
+ORDER BY margen_beneficios DESC;
+
+# 6. Mostrar detalles de los libros cuyo precio_coste sea igual al precio_coste del libro titulado 'Camboya'.
+
+SELECT 
+    l.id,
+    l.titulo,
+    a.nombre,
+    e.nombre,
+    l.stock,
+    l.precio_coste,
+    l.precio_venta
+FROM
+    libros l
+        INNER JOIN
+    autores a ON l.autor_id = a.id
+        INNER JOIN
+    editoriales e ON l.editorial_id = e.id
+WHERE
+    e.id BETWEEN 5 AND 6
+HAVING l.precio_coste = (SELECT 
+        precio_coste
+    FROM
+        libros
+    WHERE
+        titulo LIKE '%Camboya%')
+ORDER BY l.id ASC;
+
+# 7. Se desea obtener el volumen total de ventas realizadas a cada cliente en 2014.
+
+SELECT 
+    c.id,
+    c.nombre,
+    COUNT(*) numero_ventas,
+    MAX(v.importe_total) importe_máximo,
+    MIN(v.importe_total) importe_minimo,
+    SUM(v.importe_total) suma_importes
+FROM
+    clientes c
+        INNER JOIN
+    ventas v ON v.cliente_id = c.id
+WHERE
+    YEAR(v.fecha) = 2014
+GROUP BY c.id
+ORDER BY suma_importes DESC
+
+# 8. Se desea obtener el volumen total de ventas por Editorial
+
+
+
